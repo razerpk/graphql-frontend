1ad099ad26e4af307592008bc868249478718a9e
Rico Pietarinen <rico.pietarinen@gmail.com>
2019-08-31 15:05:06 +0300

    First commit, done part 8b

---
+++ src/App.js
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const ALL_AUTHORS = gql`
+{
+  allAuthors {
+    name
+    born
+    id
+    bookCount
+  }
+}`
+
+const ALL_BOOKS = gql`
+{
+  allBooks {
+    title
+    published
+    author
+    id
+    genres
+  }
+}`
+
+const CREATE_BOOK  = gql`
+  mutation addBook($title: String!, $published: Int!, $author: String!, $genres: [String!]!) {
+    addBook(
+      title: $title,
+      published: $published,
+      author: $author,
+      genres: $genres
+    ) {
+      title
+      published
+      author
+      id
+      genres
+    }
+  }
+`
+
+const EDIT_AUTHOR  = gql`
+  mutation editAuthor($name: String!, $born: Int!) {
+    editAuthor(name: $name, setBornTo: $born) {
+      name
+      id
+      born
+      bookCount
+    }
+  }
+`
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+  const authors = useQuery(ALL_AUTHORS)
+  const books = useQuery(ALL_BOOKS)
+
+  const [addBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }]
+  })
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+        result={authors}
+        editAuthor={editAuthor}
+      />
+
+      <Books
+        show={page === 'books'}
+        result={books}
+      />
+
+      <NewBook
+        show={page === 'add'}
+        addBook={addBook}
+      />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/BirthForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const BirthForm = ({ editAuthor, authors }) => {
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const submit = async (e) => {
+    e.preventDefault()
+    console.log(`Changed born year to ${born}`)
+    
+    await editAuthor({
+      variables: { name, born }
+    })
+
+    setName('')
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <select value={name} onChange={({ target }) => setName(target.value)}>
+          {authors.map(author =>
+            <option key={author.id} value={author.name}>{author.name}</option>
+          )}
+        </select>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(parseInt(target.value))}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default BirthForm
